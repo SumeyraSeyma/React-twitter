9a9f1ddf04bbb7400da1d73b7eadf7ee21b4ab38
SumeyraSeyma <114585202+SumeyraSeyma@users.noreply.github.com>
2024-09-24 11:34:10 +0300

    sidebar

--- react-twitter/src/App.js
+++ react-twitter/src/App.js
@@ -3,13 +3,11 @@ import React from 'react';
 import './output.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Sidebar/>
       <Routes>
         <Route path="/" element={<Home/>} />
