6bed0b8fc12295276105e3fa86349dcbe05eb7ae
SumeyraSeyma <114585202+SumeyraSeyma@users.noreply.github.com>
2024-09-24 09:18:47 +0300

    tailwind + home page created

--- react-twitter/src/App.js
+++ react-twitter/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import React from 'react';
-
+import './output.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/home" element={<Home/>} />
+      </Routes>
       
     </div>
   );
