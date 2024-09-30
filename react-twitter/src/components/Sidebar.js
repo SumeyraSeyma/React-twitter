aa893cf030854aad580b65b73aa5546762571b44
SumeyraSeyma <114585202+SumeyraSeyma@users.noreply.github.com>
2024-09-30 17:05:29 +0300

    responsive continue

--- react-twitter/src/components/Sidebar.js
+++ react-twitter/src/components/Sidebar.js
@@ -6,12 +6,12 @@ const Sidebar = () => {
   return (
     <div>
     
-    <div className="h-full sm:flex-row w-96 bg-black text-white fixed " id='sidebar'>
+    <div className="h-full w-96 bg-black text-white fixed " id='sidebar'>
       <ul className="space-y-2 mr-5">
         <li className="flex items-center justify-center p-2">
             <NavLink
             to="/"
-            className={`hidden 2xl:block xl:block lg:block md:block`}
+            className={`hidden 2xl:block`}
             >
             <svg className='w-6 h-6 fill-current text-white'>
                 <path d='M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z'/>
